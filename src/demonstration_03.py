191d218fc2514ea404e37a30b2233e9424ac70c6
jaren <jarensmart@yahoo.com>
2020-12-03 21:13:05 -0500

    demo3 done

--- src/demonstration_03.py
+++ src/demonstration_03.py
@@ -15,5 +15,14 @@ Notes:
 
 
 def multiply_nums(nums):
-    # Your code here
+    nums_array = nums.split(', ')
+    final_value = 1
+    for num in nums_array:
+        final_value *= int(num)
+    return final_value
 
+
+print(multiply_nums("2, 3"))
+print(multiply_nums("1, 2, 3, 4"))
+print(multiply_nums("54, 75, 453, 0"))
+print(multiply_nums("10, -2"))
